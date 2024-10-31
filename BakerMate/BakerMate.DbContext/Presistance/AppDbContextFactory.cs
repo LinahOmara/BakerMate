2e85ca36c32884b46964c16c81d3bbea0e8916f2
Linah Omara <lomara@raisaenergy.com>
2024-10-31 19:21:59 +0300

    [FEAT] add azure remote connection string

--- BakerMate/BakerMate.DbContext/Presistance/AppDbContextFactory.cs
+++ BakerMate/BakerMate.DbContext/Presistance/AppDbContextFactory.cs
@@ -13,7 +13,7 @@ namespace BakerMate.DbContext.Presistance
         public BakerMateContext CreateDbContext(string[] args = null)
         {
             var optionsBuilder = new DbContextOptionsBuilder<BakerMateContext>();
-            optionsBuilder.UseSqlServer("Server=LocalHost;Database=BakerMate;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer("Server=tcp:baker-mate-dev-db-derver.database.windows.net,1433;Initial Catalog=bakerMate;Persist Security Info=False;User ID=bakerMateDBAdmin;Password=VPZy8M8j8US$bA6;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;");
             return new BakerMateContext(optionsBuilder.Options);
         }
     }
